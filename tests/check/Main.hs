eb371d34211c79cc3b4b6b25cb0468641af6b176
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-11-17 16:20:59 +0400

    Reorganize tests.

--- tests/check/Main.hs
+++ tests/check/Main.hs
@@ -16,44 +16,15 @@ main = do
     then exitFailure
     else exitSuccess
  where
-  tests = TestList [TestLabel "trivia1" testTrivia1, TestLabel "trivia2" testTrivia2]
+  tests = TestList
+    [ TestLabel "trivia1" testTrivia1
+    , TestLabel "trivia2" testTrivia2
+    , TestLabel "dual1" testDual1
+    ]
 
 
 testTrivia1 ∷ Test
 testTrivia1 = TestCase $ do
-  install
-  check' >>= assertEqual "correct" []
-
-  install
-  rawSystem "rm" ["-rf", "file-layout-test/x"]
-  check' >>= assertEqual "deleted x" [DirectoryDoesNotExist "./x"]
-
-  install
-  rawSystem "rm" ["-rf", "file-layout-test/x/y"]
-  check' >>= assertEqual "deleted y" [DirectoryDoesNotExist "x/y"]
-
-  install
-  rawSystem "rm" ["-rf", "file-layout-test/x/z"]
-  check' >>= assertEqual "deleted z" [FileDoesNotExist "x/z"]
-
-  rawSystem "rm" ["-rf", "file-layout-test"]
-  return ()
- where
-  install = do
-    rawSystem "mkdir" ["--parents", "file-layout-test"]
-    rawSystem "mkdir" ["--parents", "file-layout-test/x/y"]
-    rawSystem "touch" ["file-layout-test/x/z"]
-
-  script =
-    directory "x" $ do
-      directory_ "y"
-      file_ "z"
-
-  check' = check script "file-layout-test"
-
-
-testTrivia2 ∷ Test
-testTrivia2 = TestCase $ do
   install
   check' >>= assertEqual "correct" []
 
@@ -94,3 +65,65 @@ testTrivia2 = TestCase $ do
       file "w" "text"
 
   check' = check script "file-layout-test"
+
+
+testTrivia2 ∷ Test
+testTrivia2 = TestCase $ do
+  rawSystem "mkdir" ["--parents", "file-layout-test"]
+  install
+  check' >>= assertEqual "correct" []
+
+  install
+  rawSystem "rm" ["-rf", "file-layout-test/x/y"]
+  check' >>= assertEqual "deleted y" [DirectoryDoesNotExist "x/y"]
+
+  install
+  rawSystem "rm" ["-rf", "file-layout-test/z"]
+  check' >>= assertEqual "deleted z" [DirectoryDoesNotExist "./z"]
+
+  install
+  rawSystem "rm" ["-rf", "file-layout-test/x/y/s"]
+  check' >>= assertEqual "deleted s" [FileDoesNotExist "x/y/s"]
+
+  install
+  writeFile "file-layout-test/z/w" "foo"
+  check' >>= assertEqual "changed z/w" [FileWrongContents "z/w" "foo"]
+
+  rawSystem "rm" ["-rf", "file-layout-test"]
+  return ()
+ where
+  install = run script "file-layout-test"
+
+  script = do
+    directory "x" $
+      directory "y" $ do
+        file_ "s"
+        file_ "v"
+    directory "z" $
+      file "w" "text"
+
+  check' = check script "file-layout-test"
+
+testDual1 ∷ Test
+testDual1 = TestCase $ do
+  rawSystem "mkdir" ["--parents", "file-layout-test"]
+  let s = do
+        directory "x" $ do
+          directory_ "xx"
+          directory "xy" $
+            file "xyx" "test1"
+          file "xz" "test2"
+          file "xz'" "test3"
+          file_ "xz''"
+        directory "y" $ do
+          file "yx" "test4"
+          file "yy" "test5"
+          file_ "yz"
+        directory_ "z"
+  test' s
+  rawSystem "rm" ["-rf", "file-layout-test"]
+  return ()
+ where
+  test' s = run' s >> check' s
+  run' s = run s "file-layout-test"
+  check' s = check s "file-layout-test" >>= assertEqual "dual" []
