02e32a8013032f59827775e8c0e4f684741381f1
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-11-29 23:49:01 +0400

    Add Layout type synonym for DL ()

--- src/System/Directory/Layout.hs
+++ src/System/Directory/Layout.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE UnicodeSyntax #-}
 module System.Directory.Layout
   ( -- * Layout declaration
-    DL, file, file_, directory, directory_
+    Layout, DL, file, file_, directory, directory_
     -- * Layout construction
   , DLMakeWarning(..), make
     -- * Layout verification
@@ -18,21 +18,25 @@ import System.Directory.Layout.Make (DLMakeWarning(..), make)
 import System.Directory.Layout.Parser (layout, layout')
 
 
+-- | Type synonym is nicer
+type Layout = DL ()
+
+
 -- | Declare file with specified contents
-file ∷ FilePath → Text → DL ()
+file ∷ FilePath → Text → Layout
 file x c = F x (Just c) (return ())
 
 
 -- | Declare empty file
-file_ ∷ FilePath → DL ()
+file_ ∷ FilePath → Layout
 file_ x = F x Nothing (return ())
 
 
 -- | Declare directory with specified listing
-directory ∷ FilePath → DL () → DL ()
+directory ∷ FilePath → Layout → Layout
 directory x d = D x d (return ())
 
 
 -- | Declare empty directory
-directory_ ∷ FilePath → DL ()
+directory_ ∷ FilePath → Layout
 directory_ x = D x (return ()) (return ())
