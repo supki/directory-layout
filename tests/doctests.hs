497e5cbafc93fabab9881367d121e20560066889
Matvey Aksenov <matvey.aksenov@gmail.com>
2013-05-01 02:13:50 +0400

    Really ad-hoc fix for doctests.

--- tests/doctests.hs
+++ tests/doctests.hs
@@ -8,5 +8,5 @@ main :: IO ()
 main = do
   x <- wordexp "cabal-dev/packages-*.conf"
   case x of
-    y:_ -> doctest ["-isrc", "-package-db=" ++ y, "src/System/Directory/Layout/Lens.hs"]
-    _   -> doctest ["-isrc", "src/System/Directory/Layout/Lens.hs"]
+    y:_ | '*' `notElem` y -> doctest ["-isrc", "-package-db=" ++ y, "src/System/Directory/Layout/Lens.hs"]
+    _                     -> doctest ["-isrc", "src/System/Directory/Layout/Lens.hs"]
