97cf38a1da9f164924dac00a03d614e796865b8f
Matvey Aksenov <matvey.aksenov@gmail.com>
2013-07-18 19:45:40 +0400

    Add convient traversals.

--- src/System/Directory/Layout/Lens.hs
+++ src/System/Directory/Layout/Lens.hs
@@ -1,12 +1,11 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RankNTypes #-}
--- | "Control.Lens" based extractors for 'DL'
+-- | "Control.Lens" based extractors for 'Layout'
 module System.Directory.Layout.Lens
-  ( -- $setup
-    text, file, directory
+  ( text, name, names, next, file, directory, node
   ) where
 
-import Control.Applicative (pure)
+import Control.Applicative ((<$>), (<*>), pure)
 
 import Control.Lens
 import Data.Text (Text)
@@ -21,7 +20,7 @@ import System.Directory.Layout.Internal (Node(..), Layout)
 -- >>> let layout = F "foo" (T "not empty" ()) (D "bar" (F "baz" (E ()) (F "quux" (T "something" ()) (E ()))) (F "swaks" (E ()) (E ())))
 
 
--- | Get 'Text' out of the current 'Layout' (if possible)
+-- | Target 'Text' from the current 'Layout' top (if possible)
 --
 -- >>> layout ^? text
 -- Nothing
@@ -29,14 +28,70 @@ import System.Directory.Layout.Internal (Node(..), Layout)
 -- Just "not empty"
 -- >>> layout ^? directory "bar" . file "quux" . text
 -- Just "something"
-text :: Prism Layout Layout Text Text
+text :: Prism' Layout Text
 text = prism' (\t -> T t ()) $ \s -> case s of
   T t _ -> Just t
   _     -> Nothing
 {-# INLINE text #-}
 
+-- | Target 'FilePath' from the current 'Layout' top (if possible)
+--
+-- >>> layout ^? name
+-- Just "foo"
+-- >>> layout ^? directory "bar" . name
+-- Just "baz"
+-- >>> layout ^? directory "quux" . name
+-- Nothing
+-- >>> layout & name .~ "boo"
+-- F "boo" (T "not empty" ()) (D "bar" (F "baz" (E ()) (F "quux" (T "something" ()) (E ()))) (F "swaks" (E ()) (E ())))
+name :: Traversal' Layout FilePath
+name f = go
+ where
+  go (E x)   = pure (E x)
+  go (T t x) = pure (T t x)
+  go (F n l x) = f n <&> \n' -> F n' l x
+  go (D n l x) = f n <&> \n' -> D n' l x
+{-# INLINE name #-}
 
--- | Look into the file in the current 'Layout' (if possible)
+-- | Target all 'Filpath's from current 'Layout' layer
+--
+-- >>> layout ^? names
+-- Just "foo"
+-- >>> layout ^.. names
+-- ["foo","bar","swaks"]
+-- >>> layout ^.. directory "bar" . names
+-- ["baz","quux"]
+-- >>> layout & directory "bar" . names %~ reverse
+-- F "foo" (T "not empty" ()) (D "bar" (F "zab" (E ()) (F "xuuq" (T "something" ()) (E ()))) (F "swaks" (E ()) (E ())))
+names :: Traversal' Layout FilePath
+names f = go
+ where
+  go (E x)   = pure (E x)
+  go (T t x) = pure (T t x)
+  go (F n l x) = (\n' x' -> F n' l x') <$> f n <*> go x
+  go (D n l x) = (\n' x' -> D n' l x') <$> f n <*> go x
+{-# INLINE names #-}
+
+-- | Target next 'Node'
+--
+-- >>> layout ^? name
+-- Just "foo"
+-- >>> layout ^? next . name
+-- Just "bar"
+-- >>> layout ^? next . next . name
+-- Just "swaks"
+-- >>> layout ^? next . next . next . name
+-- Nothing
+next :: Traversal' Layout Layout
+next f = go
+ where
+  go (E x)   = pure (E x)
+  go (T t x) = pure (T t x)
+  go (F n l x) = f x <&> \x' -> F n l x'
+  go (D n l x) = f x <&> \x' -> D n l x'
+{-# INLINE next #-}
+
+-- | Target 'Layout' under the current 'Layout' top if it happens to be a file
 --
 -- >>> layout ^? file "biz"
 -- Nothing
@@ -51,13 +106,11 @@ file k f = go
   go (T t x)    = pure (T t x)
   go (F k' l x)
     | k == k'   = indexed f k l <&> \l' -> F k' l' x
-    | otherwise = go x
-  go (D _ _ x)  = go x
-  {-# INLINE go #-}
+    | otherwise = go x <&> \x' -> F k' l x'
+  go (D n l x)  = go x <&> \x' -> D n l x'
 {-# INLINE file #-}
 
-
--- | Go into the directory in the current 'Layout' (if possible)
+-- | Target 'Layout' under the current 'Layout' top if it happens to be a directory
 --
 -- >>> layout ^? directory "foo"
 -- Nothing
@@ -68,9 +121,29 @@ directory k f = go
  where
   go (E x)      = pure (E x)
   go (T t x)    = pure (T t x)
-  go (F _ _ x)  = go x
+  go (F n l x)  = go x <&> \x' -> F n l x'
   go (D k' l x)
     | k == k'   = indexed f k l <&> \l' -> D k' l' x
-    | otherwise = go x
-  {-# INLINE go #-}
+    | otherwise = go x <&> \x' -> D k' l x'
 {-# INLINE directory #-}
+
+-- | Target 'Layout' under the current 'Layout' top
+--
+-- >>> layout ^? node "foo"
+-- Just (T "not empty" ())
+-- >>> layout ^? node "bar"
+-- Just (F "baz" (E ()) (F "quux" (T "something" ()) (E ())))
+-- >>> layout ^? node "what"
+-- Nothing
+node :: FilePath -> IndexedTraversal' FilePath Layout Layout
+node k f = go
+ where
+  go (E x)      = pure (E x)
+  go (T t x)    = pure (T t x)
+  go (F k' l x)
+    | k == k'   = indexed f k l <&> \l' -> F k' l' x
+    | otherwise = go x <&> \x' -> F k' l x'
+  go (D k' l x)
+    | k == k'   = indexed f k l <&> \l' -> D k' l' x
+    | otherwise = go x <&> \x' -> D k' l x'
+{-# INLINE node #-}
