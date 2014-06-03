04ab8938de8d267c05dabc27d898464d188fe411
Matvey Aksenov <matvey.aksenov@gmail.com>
2014-06-03 18:35:18 +0000

    Unixutils leftovers

--- test/SpecHelper.hs
+++ test/SpecHelper.hs
@@ -3,4 +3,4 @@ module SpecHelper where
 import System.IO.Temp (withSystemTempDirectory)
 
 temporary :: (FilePath -> IO a) -> IO a
-temporary = withSystemTempDirectory "directory-layout-XXXXXX"
+temporary = withSystemTempDirectory "directory-layout-"
